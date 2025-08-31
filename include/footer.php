c0a2f758199dd3124ff50a971f1819946fe5d91d
scicolone <53647201+scicolone@users.noreply.github.com>
2025-08-31 15:49:48 +0200

    Create footer.php

---
+++ include/footer.php
@@ -0,0 +1,7 @@
+</div>
+<footer>
+    &copy; 2025 A.S.D. Gi.Fra. Milazzo - Tutti i diritti riservati
+</footer>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
+</body>
+</html>
