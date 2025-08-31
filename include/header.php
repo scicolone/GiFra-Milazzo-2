3167a4f1a82ddad6bd3059bd68e22a3a031f8fd9
scicolone <53647201+scicolone@users.noreply.github.com>
2025-08-31 15:49:07 +0200

    Create header.php

---
+++ include/header.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="it">
+<head>
+    <meta charset="UTF-8">
+    <title>A.S.D. Gi.Fra. Milazzo - Gestione Sportiva</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="icon" href="img/logo.png">
+</head>
+<body>
+<header>
+    <div class="container">
+        <img src="img/logo.png" alt="Logo Gi.Fra. Milazzo" class="logo">
+        <h1>A.S.D. GI.FRA. MILAZZO</h1>
+        <p class="lead">Stagione Corrente: 2025/26</p>
+    </div>
+</header>
+<div class="container">
