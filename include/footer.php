e6af02535be26bd3e3dc0022bf4eeb826f20ef64
scicolone <53647201+scicolone@users.noreply.github.com>
2025-09-01 20:34:23 +0200

    Update footer.php

--- include/footer.php
+++ include/footer.php
@@ -1,5 +1,5 @@
 </div>
-<footer>
+<footer class="text-center py-3 text-muted">
     &copy; 2025 A.S.D. Gi.Fra. Milazzo - Tutti i diritti riservati
 </footer>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
