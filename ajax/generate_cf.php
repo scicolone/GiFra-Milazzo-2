93dbd3c709e1b48969c79678a922adf70bc19aaa
scicolone <53647201+scicolone@users.noreply.github.com>
2025-09-02 10:12:26 +0200

    Update generate_cf.php

--- ajax/generate_cf.php
+++ ajax/generate_cf.php
@@ -13,34 +13,45 @@ if (!$nome || !$cognome || !$data || !$sesso || !$codice_catastale) {
 }
 
 function generateCodiceFiscale($nome, $cognome, $data_nascita, $sesso, $codice_catastale) {
-    $mesi = ['A', 'B', 'C', 'D', 'E', 'H', 'L', 'M', 'P', 'R', 'S', 'T'];
+    $mesi = [1 => 'A', 2 => 'B', 3 => 'C', 4 => 'D', 5 => 'E', 6 => 'H', 7 => 'L', 8 => 'M', 9 => 'P', 10 => 'R', 11 => 'S', 12 => 'T'];
     
-    $cognome_cf = estraiConsonantiVocali($cognome, 3);
-    $nome_cf = estraiConsonantiVocali($nome, 3);
+    // Estrai consonanti e vocali per cognome
+    $cognome_cf = estraiCF($cognome, 3);
     
-    $data = new DateTime($data_nascita);
-    $anno = substr($data->format('Y'), -2);
-    $mese = $mesi[$data->format('n') - 1];
-    $giorno = (int)$data->format('j');
+    // Estrai consonanti e vocali per nome
+    $nome_cf = estraiCF($nome, 3);
     
+    // Data di nascita
+    $data_obj = new DateTime($data_nascita);
+    $anno = substr($data_obj->format('Y'), -2);
+    $mese = $mesi[(int)$data_obj->format('n')];
+    $giorno = (int)$data_obj->format('j');
+    
+    // Se femmina, aggiungi 40
     if ($sesso === 'F') {
         $giorno += 40;
     }
     $giorno_cf = str_pad($giorno, 2, '0', STR_PAD_LEFT);
     
+    // Codice catastale
     $comune_cf = substr(strtoupper($codice_catastale), 0, 4);
+    
+    // Componi codice fiscale parziale
     $cf_parziale = $cognome_cf . $nome_cf . $anno . $mese . $giorno_cf . $comune_cf;
-    $controllo = 'X'; // Semplificato
+    
+    // Carattere di controllo (semplificato)
+    $controllo = 'X';
     
     return $cf_parziale . $controllo;
 }
 
-function estraiConsonantiVocali($stringa, $lunghezza) {
+function estraiCF($stringa, $lunghezza) {
     $stringa = strtoupper($stringa);
     $vocali = ['A', 'E', 'I', 'O', 'U'];
     $consonanti = '';
     $vocali_str = '';
     
+    // Separa consonanti e vocali
     for ($i = 0; $i < strlen($stringa); $i++) {
         $char = $stringa[$i];
         if (in_array($char, $vocali)) {
@@ -50,6 +61,7 @@ function estraiConsonantiVocali($stringa, $lunghezza) {
         }
     }
     
+    // Componi il risultato
     $risultato = $consonanti . $vocali_str;
     return str_pad(substr($risultato, 0, $lunghezza), $lunghezza, 'X');
 }
