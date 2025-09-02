55e90008c49447c1010f36c0adf4cca9a5cf21c0
scicolone <53647201+scicolone@users.noreply.github.com>
2025-09-02 18:28:01 +0200

    Update search_comuni.php

--- ajax/search_comuni.php
+++ ajax/search_comuni.php
@@ -2,6 +2,7 @@
 require_once '../config.php';
 
 header('Content-Type: application/json');
+header('Access-Control-Allow-Origin: *');
 
 if (!isset($_GET['term']) || strlen($_GET['term']) < 2) {
     echo json_encode([]);
